26db1144bbb76988e164ecd8be50ec0e607dbfac
Cnchinda <85324838+Cnchinda@users.noreply.github.com>
2022-11-18 10:20:50 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,6 @@
 
 provider "aws" {
     region = "us-east-2"
-    profile = {
-        AWS_Access_Key_ID = ${{secrets.AWS_Access_Key_ID}}
-        AWS_Secret_Access_Key = ${{secrets.AWS_SECRET_ACCESS_KEY}}
-   }
+    profile = ""
   
 }
