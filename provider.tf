beebfab293b31fd312dae62f2ea7f66f592c822c
Cnchinda <85324838+Cnchinda@users.noreply.github.com>
2022-11-18 10:38:31 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@
   #  profile = ""
 
 provider "aws" {
-  region     = "us-west-2"
+  region     = "us-east-1"
   access_key = "AKIARTP7QI2XL6TSWJR2"
   secret_key = "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"
 }
