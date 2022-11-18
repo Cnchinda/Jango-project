ac5f6b866ac78c765b43d44ef73c251f58b1f035
Cnchinda <85324838+Cnchinda@users.noreply.github.com>
2022-11-18 10:29:50 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,3 +4,9 @@
   #  profile = ""
   
 }
+
+provider "aws" {
+   region = "AWS REGION"
+    AWS_Access_Key_ID = "AKIARTP7QI2XL6TSWJR2"
+    AWS_Secret_Access_Key = "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"
+   }
