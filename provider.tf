6f04d560ae2c26f01850a06e86a9e69be725b653
Cnchinda <85324838+Cnchinda@users.noreply.github.com>
2022-11-21 13:35:17 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,8 +3,8 @@
    # region = "us-east-2"
   #  profile = ""
 
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIARTP7QI2XL6TSWJR2"
-  secret_key = "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"
+#provider "aws" {
+ # region     = "us-east-1"
+  #access_key = ""
+  #secret_key = ""
 }
